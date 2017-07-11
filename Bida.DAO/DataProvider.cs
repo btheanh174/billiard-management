d6bfcf4e59456717bfbd09a8125a9c52e7fba6bc
White <btheanh174@gmail.com>
2017-07-11 21:35:22 +0700

    first commit

---
+++ Bida.DAO/DataProvider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bida.DAO
+{
+    class DataProvider
+    {
+        public const string onStr = "Data Source = DESKTOP-8H4958A;Initial Catalog = Bida;Integrated Security = SSPI";
+        SqlConnection conn;
+        private SqlCommand command;
+
+        public DataProvider()
+        {
+            conn = new SqlConnection(onStr);
+        }
+
+        public void Connect()
+        {
+            conn.Open();
+        }
+
+        public void Disconnect()
+        {
+            conn.Close();
+        }
+
+        public SqlDataReader ExcuteReader(String sql)
+        {
+            command = new SqlCommand(sql, conn);
+            return command.ExecuteReader();
+        }
+        public void executeNonQuery(string sql)
+        {
+            this.command = new SqlCommand(sql, conn);
+             this.command.ExecuteNonQuery();
+        }
+        
+
+
+    }
+}
